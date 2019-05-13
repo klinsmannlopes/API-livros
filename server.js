11fd3f7e905b8c9c3a847a61ddbaa6eecbb57731
klinsmann lopes <lopesklisman@gmail.com>
2019-05-13 14:04:17 -0300

    instalando depêndencias

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
-    resp.end(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body>
-        </html>
-    `);
+    let = html = '';
+    if(req.url == '/') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body>
+            </html>
+        `
+    } else if (req.url == '/livros') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Lista Livros </h1>
+                </body>
+            </html>
+        `
+    }
+    resp.end(html);
 });
 servidor.listen(3000);
