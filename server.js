6611e8db98e5826aae710aaeb64d37561bf64f1f
klinsmann lopes <lopesklisman@gmail.com>
2019-05-13 14:26:11 -0300

    encapsulando express

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
-const http = require('http');
+const app = require('./src/config/custom-express');
 
-const servidor = http.createServer(function (req, resp) {
-    let = html = '';
-    if(req.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body>
-            </html>
-        `
-    } else if (req.url == '/livros') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Lista Livros </h1>
-                </body>
-            </html>
-        `
-    }
-    resp.end(html);
+app.listen(3000, function() {
+    console.log(`servidor rodando 300`);
 });
-servidor.listen(3000);
+
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body>
+            </html>
+        `
+    );
+});
+
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `
+            <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body>
+            </html>
+        `
+    );
+});
