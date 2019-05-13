80282acfa224a7c77d373d305f22d61df1d18122
klinsmann lopes <lopesklisman@gmail.com>
2019-05-13 14:36:34 -0300

    sparenado responsabilidades

--- server.js
+++ server.js
@@ -3,33 +3,3 @@ const app = require('./src/config/custom-express');
 app.listen(3000, function() {
     console.log(`servidor rodando 300`);
 });
-
-app.get('/', function(req, resp) {
-    resp.send(
-        `
-            <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body>
-            </html>
-        `
-    );
-});
-
-app.get('/livros', function(req, resp) {
-    resp.send(
-        `
-            <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body>
-            </html>
-        `
-    );
-});
